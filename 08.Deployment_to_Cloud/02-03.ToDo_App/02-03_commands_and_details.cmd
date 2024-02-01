6868d1ef334911096fb5c5067b1ae8e04c227a73
Kristiyan Bonev <k.s.bonev@gmail.com>
2024-02-01 19:10:32 +0200

    Forgot one file with notes for an app deployed in Azure

---
+++ 08.Deployment_to_Cloud/02-03.ToDo_App/02-03_commands_and_details.cmd
@@ -0,0 +1,9 @@
+echo off
+
+project name: todoappkris
+project pass: 
+
+database account name: todoappkris-server
+database name: todoappkris-database
+
+AZURE_COSMOS_CONNECTIONSTRING: mongodb://todoappkris-server:BFMtIH4Z2JpoUBGnHoEtO113F8aSvDviUaeLLRUiXwqnrbwxaFWG9chz5xXMJ3oLfFGEp0Jw5nmJACDbDQ410Q==@todoappkris-server.mongo.cosmos.azure.com:10255/todoappkris-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@todoappkris-server@
