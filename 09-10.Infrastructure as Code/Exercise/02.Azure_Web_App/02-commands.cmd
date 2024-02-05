088953265c286f9f7282a0a8e3ede5f407b15080
Kristiyan Bonev <k.s.bonev@gmail.com>
2024-02-05 19:32:39 +0200

    IAC ex02

---
+++ 09-10.Infrastructure as Code/Exercise/02.Azure_Web_App/02-commands.cmd
@@ -0,0 +1,13 @@
+::Download Azure.exe into current dir
+terraform init
+
+::Check if .tf file is valid
+terraform fmt
+terraform validate
+
+::Review what the file is going to do and then execute
+terraform plan
+terraform apply
+
+::Kill everything
+terraform destroy
